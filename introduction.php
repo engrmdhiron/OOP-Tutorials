8d07f36519184387c47c1d7fb991fc6b57de988c
proghiron <prog.mdhiron@gmail.com>
2018-12-29 00:08:42 +0600

    Added what is the php

--- introduction.php
+++ introduction.php
@@ -4,6 +4,9 @@
 	<title>PHP Tutorials</title>
 </head>
 <body>
-<h1 style="text-align:center; color:blue; "><u>PHP Introductionddddd</u> </h1>
+<h1 style="text-align:center; color:blue; "><u>PHP Introduction</u> </h1>
+<p>01) PHP is the server site scripting language</p>
+<p>02) PHP is the opensource scripting language</p>
+<p>03) PHP free download and use</p>
 </body>
 </html>
