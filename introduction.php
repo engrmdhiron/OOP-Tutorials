27bde49aa18573727f3540f408e227bc64f778b4
proghiron <prog.mdhiron@gmail.com>
2018-12-29 22:29:27 +0600

    commited 29/12/18

--- introduction.php
+++ introduction.php
@@ -5,8 +5,13 @@
 </head>
 <body>
 <h1 style="text-align:center; color:blue; "><u>PHP Introduction</u> </h1>
-<p>01) PHP is the server site scripting language</p>
-<p>02) PHP is the opensource scripting language</p>
-<p>03) PHP free download and use</p>
+<h3 style="color:green;"><u>What is PHP?</u></h3>
+<p>01) PHP is the Hypertext Preprocessor</p>
+<p>02) PHP is the server site scripting language</p>
+<p>03) PHP is the open-source scripting language</p>
+<p>04) PHP free download and use</p>
+<p>05) PHP script are executed on the server</p>
+
+<h3 style="color:blue;"><u>What can php do?</u></h3>
 </body>
 </html>
