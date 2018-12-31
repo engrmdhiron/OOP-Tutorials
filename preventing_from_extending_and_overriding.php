ac4392f2ed96de37580618c614cc6faff3b12905
proghiron <prog.mdhiron@gmail.com>
2019-01-01 04:56:48 +0600

    Used final keywords in class and methods

--- preventing_from_extending_and_overriding.php
+++ preventing_from_extending_and_overriding.php
@@ -10,11 +10,40 @@
    <h1 style="color:blue; text-align:center;"><u>Preventing from extending and overriding</u>
    <br>
    <a href="index.php"><button>Back to Index</button></a></h1>
+
+   <h2>How to work final keyword: final keyword will be stop extends class and overrite function</h2>
     
 </body>
 </html>
 
 <?php
+
+class UserData {
+    public $name;
+    public $age;
+
+    public function __construct($name, $age){
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function userInfo() {
+        echo 'My name is '.$this->name.' and age is '.$this->age.' Years';
+    }
+}
+
+class Admin extends UserData {
+
+    public final function userInfo() {
+        echo 'My name is '.$this->name.' and age is '.$this->age.' Years';
+    }
+    
+}
+
+$name  = 'Md.Hiron Mollik (Sagor)';
+$age = 27;
+$info = new UserData($name, $age);
+$info->userInfo();
 
 
 
