aa5fa931f501153e6c143fa11956a21bde20a805
proghiron <prog.mdhiron@gmail.com>
2019-01-12 02:12:11 +0600

    Created abstract class

--- abstract.php
+++ abstract.php
@@ -7,46 +7,20 @@
     <title>Document</title>
 </head>
 <body>
-   <h1 style="color:blue; text-align:center;"><u>Abstract</u>
-   <br>
-   <a href="index.php"><button>Back to Index</button></a></h1>
-    
+    <h1 style="color:blue; text-align:center;"><u>Abstract</u>
+    <br>
+    <a href="index.php"><button>Back to Index</button></a></h1>
+    <p>Classes defined as abstract may not be instantiated, and any class that contains at least one abstract method must also be abstract. Methods defined
+    When inheriting from an abstract class, all methods marked abstract in the parent's class declaration must be defined by the child; additionally, these methods must be defined with the same (or a less restricted) visibility. For example, if the abstract method is defined as protected, the function implementation must be defined as either protected or public, but not private. Furthermore the signatures of the methods must match, i.e. the type hints and the number of required arguments must be the same. For example, if the child class defines an optional argument, where the abstract method's signature does not, there is no conflict in the signature. This also applies to constructors as of PHP 5.4. Before 5.4 constructor signatures could differ. </p>
+    <blockquote>
+        <pre>
+            <code>
+                
+            </code>
+        </pre>
+    </blockquote>
+    <?php 
+        
+    ?>
 </body>
 </html>
-
-<?php
-class UserData {
-
-    public $name;
-    public $email;
-
-    public function display($name, $email){
-        echo "User name is ".$this->name=$name.' <br/> User email is '.$this->email=$email;
-    }
-}
-
-
-class Admin extends UserData{
-    public $age;
-
-    public function display($name, $email){
-        echo "Name is ".$this->name=$name.'<br/> Email is '.$this->email=$email.'<br/> Age is '.$this->age.' Years';
-    }
-
-}
-
-
-
-$name = "Md.Hiron Mollik";
-$email = "prog.mdhiron@gmail.com";
-$age = 27;
-// $userData = new UserData;
-// $userData->display($name, $email);
-
-$adminData = new Admin;
-$adminData->age=$age;
-$adminData->display($name, $email);
-
-
-
-?>
